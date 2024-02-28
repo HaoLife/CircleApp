a96a5f07947d50fd01f016840aa259e39c34268d
w4384 <w4384@HAOZI>
2024-02-28 16:07:43 +0800

    授权问题

--- CircleApp/App.cs
+++ CircleApp/App.cs
@@ -65,7 +65,7 @@ public class App : Microsoft.UI.Xaml.Application
                     services.AddTransient<DelegatingHandler, DebugHttpHandler>();
 #endif
 
-#if DEBUG
+#if !DEBUG
 
                     services.AddTransient<ITrendService, MockTrendService>()
                         .AddTransient<IContactService, MockContactService>()
@@ -81,46 +81,46 @@ public class App : Microsoft.UI.Xaml.Application
                     services.AddSingleton<IWeatherCache, WeatherCache>()
                         .AddRefitClient<IApiClient>(context);
                 })
-                .UseAuthentication(auth =>
-                {
-                    auth.AddCustom(custom =>
-                    {
-                        custom.Login((sp, dispatcher, credentials, cancellationToken) =>
-                        {
-                            // TODO: Write code to process credentials that are passed into the LoginAsync method
-                            if (credentials?.TryGetValue(nameof(LoginViewModel.Username), out var username) ?? false &&
-                                !username.IsNullOrEmpty())
-                            {
-                                // Return IDictionary containing any tokens used by service calls or in the app
-                                credentials ??= new Dictionary<string, string>();
-                                credentials[TokenCacheExtensions.AccessTokenKey] = "SampleToken";
-                                credentials[TokenCacheExtensions.RefreshTokenKey] = "RefreshToken";
-                                credentials["Expiry"] = DateTime.Now.AddMinutes(5).ToString("g");
-                                return ValueTask.FromResult<IDictionary<string, string>?>(credentials);
-                            }
+                //.UseAuthentication(auth =>
+                //{
+                //    auth.AddCustom(custom =>
+                //    {
+                //        custom.Login((sp, dispatcher, credentials, cancellationToken) =>
+                //        {
+                //            // TODO: Write code to process credentials that are passed into the LoginAsync method
+                //            if (credentials?.TryGetValue(nameof(LoginViewModel.Username), out var username) ?? false &&
+                //                !username.IsNullOrEmpty())
+                //            {
+                //                // Return IDictionary containing any tokens used by service calls or in the app
+                //                credentials ??= new Dictionary<string, string>();
+                //                credentials[TokenCacheExtensions.AccessTokenKey] = "SampleToken";
+                //                credentials[TokenCacheExtensions.RefreshTokenKey] = "RefreshToken";
+                //                credentials["Expiry"] = DateTime.Now.AddMinutes(5).ToString("g");
+                //                return ValueTask.FromResult<IDictionary<string, string>?>(credentials);
+                //            }
 
-                            // Return null/default to fail the LoginAsync method
-                            return ValueTask.FromResult<IDictionary<string, string>?>(default);
-                        }).Refresh((sp, tokenDictionary, cancellationToken) =>
-                        {
-                            if ((tokenDictionary?.TryGetValue(TokenCacheExtensions.RefreshTokenKey, out var refreshToken) ?? false) &&
-                                !refreshToken.IsNullOrEmpty() &&
-                                (tokenDictionary?.TryGetValue("Expiry", out var expiry) ?? false) &&
-                                DateTime.TryParse(expiry, out var tokenExpiry) &&
-                                tokenExpiry > DateTime.Now)
-                            {
-                                // Return IDictionary containing any tokens used by service calls or in the app
-                                tokenDictionary ??= new Dictionary<string, string>();
-                                tokenDictionary[TokenCacheExtensions.AccessTokenKey] = "NewSampleToken";
-                                tokenDictionary["Expiry"] = DateTime.Now.AddMinutes(5).ToString("g");
-                                return ValueTask.FromResult<IDictionary<string, string>?>(tokenDictionary);
-                            }
+                //            // Return null/default to fail the LoginAsync method
+                //            return ValueTask.FromResult<IDictionary<string, string>?>(default);
+                //        }).Refresh((sp, tokenDictionary, cancellationToken) =>
+                //        {
+                //            if ((tokenDictionary?.TryGetValue(TokenCacheExtensions.RefreshTokenKey, out var refreshToken) ?? false) &&
+                //                !refreshToken.IsNullOrEmpty() &&
+                //                (tokenDictionary?.TryGetValue("Expiry", out var expiry) ?? false) &&
+                //                DateTime.TryParse(expiry, out var tokenExpiry) &&
+                //                tokenExpiry > DateTime.Now)
+                //            {
+                //                // Return IDictionary containing any tokens used by service calls or in the app
+                //                tokenDictionary ??= new Dictionary<string, string>();
+                //                tokenDictionary[TokenCacheExtensions.AccessTokenKey] = "NewSampleToken";
+                //                tokenDictionary["Expiry"] = DateTime.Now.AddMinutes(5).ToString("g");
+                //                return ValueTask.FromResult<IDictionary<string, string>?>(tokenDictionary);
+                //            }
 
-                            // Return null/default to fail the Refresh method
-                            return ValueTask.FromResult<IDictionary<string, string>?>(default);
-                        });
-                    }, name: "CustomAuth");
-                })
+                //            // Return null/default to fail the Refresh method
+                //            return ValueTask.FromResult<IDictionary<string, string>?>(default);
+                //        });
+                //    }, name: "CustomAuth");
+                //})
                 .ConfigureServices((context, services) =>
                 {
 
@@ -145,21 +145,21 @@ public class App : Microsoft.UI.Xaml.Application
 
         Host = await builder.NavigateAsync<Shell>(initialNavigate: async (services, navigator) =>
         {
-            var auth = services.GetRequiredService<IAuthenticationService>();
+//            var auth = services.GetRequiredService<IAuthenticationService>();
 
-            var authenticated = await auth.LoginAsync();
+//            var authenticated = await auth.LoginAsync();
 
-#if DEBUG
-            authenticated = true;
-#endif
-            if (authenticated)
-            {
+//#if DEBUG
+//            authenticated = true;
+//#endif
+//            if (authenticated)
+//            {
                 await navigator.NavigateViewModelAsync<FullViewModel>(this, qualifier: Qualifiers.Nested);
-            }
-            else
-            {
-                await navigator.NavigateViewModelAsync<GuideViewModel>(this, qualifier: Qualifiers.Nested);
-            }
+            //}
+            //else
+            //{
+            //    await navigator.NavigateViewModelAsync<GuideViewModel>(this, qualifier: Qualifiers.Nested);
+            //}
         });
     }
 
--- CircleApp/ViewModels/ShellViewModel.cs
+++ CircleApp/ViewModels/ShellViewModel.cs
@@ -2,26 +2,26 @@ namespace CircleApp.ViewModels;
 
 public partial class ShellViewModel : ObservableObject
 {
-    private readonly IAuthenticationService authentication;
+    //private readonly IAuthenticationService authentication;
     private readonly INavigator _navigator;
 
     public ShellViewModel(
-        IAuthenticationService authentication,
+        //IAuthenticationService authentication,
         INavigator navigator)
     {
-        this.authentication = authentication;
+        //this.authentication = authentication;
         _navigator = navigator;
-        this.authentication.LoggedOut += LoggedOut;
-        //_ = Start();
+        //this.authentication.LoggedOut += LoggedOut;
+        _ = Start();
     }
 
-    //public async Task Start()
-    //{
-    //    await _navigator.NavigateRouteAsync(this, "Full");
-    //    //await _navigator.NavigateViewModelAsync<MainModel>(this);
-    //    //await _navigator.NavigateViewModelAsync<GuideModel>(this);
-    //    //await _navigator.NavigateViewModelAsync<PrivacySettingModel>(this);
-    //}
+    public async Task Start()
+    {
+        await _navigator.NavigateRouteAsync(this, "Full");
+        //await _navigator.NavigateViewModelAsync<MainModel>(this);
+        //await _navigator.NavigateViewModelAsync<GuideModel>(this);
+        //await _navigator.NavigateViewModelAsync<PrivacySettingModel>(this);
+    }
 
 
     private async void LoggedOut(object? sender, EventArgs e)
